a56572bf7be15e9a4fadc13fd1e4b17f885e4785
dilsoft <hsamithdilhara@gmail.com>
2019-05-09 21:08:49 +0530

    initial commit

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
+
+import App from '../App';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it('shows a comment box', () => {
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
+});
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import CommentBox from '../CommentBox';
+import { mount } from 'enzyme';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+it('has a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
+
+describe('textarea', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  });
+
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
+
+  it('when form is submitted, textarea gets emptied', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
